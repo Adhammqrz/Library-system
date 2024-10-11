05317a37d14e9e605277ca7528b2e4f09bc535c0
Adhammqrz <adhammuqriz@gmail.com>
2024-10-11 10:49:27 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
+
+int validation(User *data,int totaluser, char *inputusername ,char *inputpass){
+        for (int i=0; i<totaluser; i++)
+        {
+            if (strcmp(data[i].username, inputusername) == 0 && strcmp(data[i].password, inputpass) == 0)
+                return data[i].role;
+            }
+        return -1;
+}
+
+
+int loaduserdata(char *filename, User *data){
+    FILE *fptr=fopen(filename,"r");
+
+    if (fptr==NULL){
+        printf("Error: no %s found!", filename);
+        return 0;
+    }
+
+    int counter=0;
+    char line[100];
+
+    while (fgets(line, sizeof(line),fptr)){
+        sscanf(line,"%s %s %d", data[counter].username, data[counter].password, &data[counter].role);
+        counter++;
+    }
+    fclose(fptr);
+    return counter;
+}
 
 int main()
 {
