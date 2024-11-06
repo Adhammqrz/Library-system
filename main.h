ea5e04b95c2ee624a4391393fa12a46c2fdb556f
Adhammqrz <adhammuqriz@gmail.com>
2024-11-06 11:29:12 +0800

    6/11/2024 changes

--- main.h
+++ main.h
@@ -15,13 +15,20 @@ typedef struct{
 } User;
 
 typedef struct{
+    char shelfID[50];
     char id[50];
     char title[50];
     char author[50];
     char genre[50];
-    int quantity;
+    char status[50];
     char type[50];
 } Book;
+
+typedef struct {
+    char id[50];
+    char title[50];
+    int count;
+} popular_book;
 
 typedef struct{
     char name[100];
@@ -31,9 +38,18 @@ typedef struct{
     char status[50];
 } Member;
 
+typedef struct{
+    char username[50];
+    char bookid[50];
+    char booktitle[50];
+    char duedate[50];
+    char status[50];
+    int fines;
+}BorrowBook;
 
 int validation(char *inputusername, char *inputpass);
 
 #endif // MAIN_H
+
 
 
